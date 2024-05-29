0d0320157f754322e6a153521da837bf3877a672
Anne-Lii <145770813+Anne-Lii@users.noreply.github.com>
2024-05-29 13:45:26 +0200

    lagt till hamburgemeny för mindre skärmar

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -587,11 +587,40 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 //code written by Anne-Lii Hansen VT 2024
 "use strict";
 document.addEventListener("DOMContentLoaded", ()=>{
+    let menuVisible = true; // Initialize menu as visible
+    function handleResize() {
+        const navMenu = document.getElementById("nav-menu");
+        if (window.innerWidth >= 1000) {
+            navMenu.style.display = "block";
+            menuVisible = true;
+        }
+    }
+    window.addEventListener("resize", handleResize);
     window.addEventListener("scroll", function() {
         let nav = document.querySelector("nav");
         if (this.window.scrollY > 0) nav.classList.add("nav-scroll");
         else nav.classList.remove("nav-scrolled");
     });
+    document.getElementById("open-menu").addEventListener("click", ()=>{
+        const navMenu = document.getElementById("nav-menu");
+        if (!menuVisible) {
+            navMenu.style.display = "block";
+            menuVisible = true;
+        } else {
+            navMenu.style.display = "none";
+            menuVisible = false;
+        }
+    });
+    document.getElementById("close-menu").addEventListener("click", ()=>{
+        const navMenu = document.getElementById("nav-menu");
+        if (window.innerWidth < 1000) {
+            if (menuVisible) {
+                navMenu.style.display = "none";
+                menuVisible = false;
+            }
+        }
+    });
+    handleResize(); // Call handleResize initially to set menu display on page load
 });
 
 },{}]},["j2YDk","1SICI"], "1SICI", "parcelRequire4e03")
